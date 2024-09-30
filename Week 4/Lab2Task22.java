63778027a2efbeb36abd8b6e5d3a5ada2b200dcb
stan0930 <142971218+stan0930@users.noreply.github.com>
2024-09-30 10:49:11 +0800

    Lab2Task22.java

---
+++ Week 4/Lab2Task22.java
@@ -0,0 +1,26 @@
+public class Lab2Task22 {
+    public static void main(String[] args) {
+        int x = 1;
+        int a = 3;
+
+        switch (a) {
+            case 1:
+                x += 5;
+                break;
+            case 2:
+                x += 10;
+                break;
+            case 3:
+                x += 16;
+                break;
+            case 4:
+                x += 34;
+                break;
+            default:
+              
+                break;
+        }
+
+        System.out.println(x);
+    }
+}
