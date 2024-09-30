5e459880277df36d9e8c4a36a39f0a64421b1636
stan0930 <142971218+stan0930@users.noreply.github.com>
2024-09-30 09:19:05 +0800

    Update Lab2Task1.java

--- Week 4/Lab2Task1.java
+++ Week 4/Lab2Task1.java
@@ -1,1 +1,8 @@
-
+public class Test {
+    public static void main(String[] args) {
+        double miles = 100;
+        final double KILOMETERS_PER_MILE = 1.609;
+        double kilometers = miles * KILOMETERS_PER_MILE;
+        System.out.println("Kilometers: " + kilometers);
+    }
+}
