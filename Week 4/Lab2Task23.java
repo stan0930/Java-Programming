d35bfb72633347c793ebb7e5190ce81b450736ba
stan0930 <142971218+stan0930@users.noreply.github.com>
2024-09-30 10:50:05 +0800

    Lab2Task23.java

---
+++ Week 4/Lab2Task23.java
@@ -0,0 +1,10 @@
+public class Lab2Task23 {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 8;
+
+        int max = (num1 > num2) ? num1 : num2;
+
+        System.out.println(max);
+    }
+}
