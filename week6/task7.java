7bab164cfdc54388affd782cc9e8fee41af93ecd
stan0930 <142971218+stan0930@users.noreply.github.com>
2024-10-14 10:13:19 +0800

    Add files via upload

---
+++ week6/task7.java
@@ -0,0 +1,26 @@
+package week6;
+
+public class task7 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int j = 0;
+
+        for (int i = 0; i < 10; i++) {
+            sum += i;
+
+            if (i < j) {
+                System.out.println(i);
+            } else {
+                System.out.println(j);
+            }
+        }
+
+        while (j < 10) {
+            j++;
+        }
+
+        do {
+            j++;
+        } while (j < 10);
+    }
+}
